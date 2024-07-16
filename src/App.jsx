7aa06e83cbeb6f2a11a848f1e4989a6c0f4e2bc2
Tarang <tarangsultania54@gamil.com>
2024-07-16 13:37:51 +0530

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Todo</h1>
-    </>
+      <AddTodo />
+      <Todos/>
+    </div>
   )
 }
 
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -2,8 +2,8 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
     todos: [{
-        id: 1, 
-        text: "Hello World"
+        id: "", 
+        text: ""
     }]
 }
 
@@ -11,7 +11,22 @@ export const todoSlice = createSlice({
     name: "todo",
     initialState,
     reducers: {
-        addTodo: (state, action) => {},
-        removeTodo: () => {}
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), //better than Date.now()
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id ? action.payload.todo : todo)
+        }
     }
 })
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
